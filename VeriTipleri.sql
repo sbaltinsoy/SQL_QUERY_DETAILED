f26873f879bbadc9769a29e18effa951792f530f
sbaltinsoy <serhatburakaltinsoy@gmail.com>
2021-09-22 16:11:45 +0300

    Veri tipleri

---
+++ VeriTipleri.sql
@@ -0,0 +1,21 @@
+-- SQL Veri Tipleri ------------------------------------------------------------------------
+
+-- char(10) -> 10 karakterlik bellekte yer isgal eder doldurmasak bile
+-- varchar(10) -> maximum 10 karakterlik alan saglar girilen karakter kadar bellekte yer isgal eder
+-- nchar ve nvarchar -> latin alfabesi disindaki dilleride dahil eder
+-- date -> tarih
+-- datetime -> tarih zaman
+-- datetimeoffset() ->
+-- decimal(_ , _) -> ilk bosluga yazilan sayi girilecek olan karakter sayisinin yani toplam basamak degerini ifade eder
+-- 2. boþluk ýse virgulden sonra kac basamak sayi girelebilecegini temsil etmektedir
+-- float -> ondalikli bir veri tipi decimal e gore daha az yer kaplar
+-- image -> kullanilmasi tercih edilmiyor db i sisirir. resim ve video gibi dosyalari saklamak icin kullanilir
+-- genedle dosya yollari tutulur
+-- int -> tam sayilar icin kullaniliyor
+-- Smalldatetime -> 1900 lerden 2070 lere kadar olan zaman araligi icin kullanilir
+-- Smallint -> += 32.650 araliginda olan veri tipi
+-- smallmoney
+-- text -> uzun metinler icin kullaniliyor
+-- Tinyint -> 0-255
+
+
